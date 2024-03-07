8ec25a6b71ecafeef4ca00cf0799e0ed262cde74
bjaouiala <bjaouiala3@gmail.com>
2024-03-07 15:37:57 +0100

    verifyAccount

--- android/app/src/main/java/com/example/myapplication/VerifyCompte.java
+++ android/app/src/main/java/com/example/myapplication/VerifyCompte.java
@@ -5,32 +5,58 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Verify_compte extends AppCompatActivity {
-    private Toolbar toolbar;
+import com.example.myapplication.config.RetrofitClient;
+import com.example.myapplication.service.ConfirmAccountService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class VerifyCompte extends AppCompatActivity {
+
+    private EditText code;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_compte);
+        code = findViewById(R.id.code);
+        button = findViewById(R.id.envoyer);
 
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setTitle("");
-        }
+        button.setOnClickListener(view->verifyAccount());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:
-                finish();
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+
+
+    public void verifyAccount(){
+        String CodeConfirmation = code.getText().toString();
+        ConfirmAccountService confirmAccountService = RetrofitClient.getRetrofitInsantce().create(ConfirmAccountService.class);
+        Call<String> call = confirmAccountService.verifyAccount(CodeConfirmation);
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful() && response.body() != null){
+                    Intent i = new Intent(VerifyCompte.this,LoginActivity.class);
+                    startActivity(i);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.e("onFailure", "Network error", t);
+                Toast.makeText(VerifyCompte.this, "Network error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
--- android/app/src/main/java/com/example/myapplication/service/ConfirmAccountService.java
+++ android/app/src/main/java/com/example/myapplication/service/ConfirmAccountService.java
@@ -1,2 +1,12 @@
-package com.example.myapplication.service;public interface ConfirmAccountService {
+package com.example.myapplication.service;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.PUT;
+
+public interface ConfirmAccountService {
+    @FormUrlEncoded
+    @PUT("auth")
+    Call<String> verifyAccount(@Field("CodeConfirmation")String CodeConfirmation);
 }
